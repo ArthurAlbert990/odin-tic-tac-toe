40357326b33b78dec47707b5abc947806b99e651
Arthur <arthur.albert990@gmail.com>
2022-10-10 14:27:33 -0300

    Start basic functions.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('js working')
+console.log('js working');
+
+
+//saves the current board state:
+let gameboard = [];
+
+
+ function player(name){
+    /** 
+    *Factory function to create new players.
+    */
+    return {name};
+}
+
+
+function markCell(){
+    //obtain the cell 'index' from the grid
+    //if cell is empty, mark with x or o.
+    //check if won
+    //other player turn.
+}
