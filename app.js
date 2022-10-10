402dc513612d8335d97f2dc82c2b3771d8d20978
Arthur <arthur.albert990@gmail.com>
2022-10-10 15:24:22 -0300

    Update functions.

--- app.js
+++ app.js
@@ -13,9 +13,20 @@ let gameboard = [];
 }
 
 
-function markCell(){
+function markCell(e){
     //obtain the cell 'index' from the grid
     //if cell is empty, mark with x or o.
     //check if won
     //other player turn.
+    console.log('markcell')
+    console.log(e.target.getAttribute('data-position'))
 }
+
+//elements
+const cell = document.querySelectorAll('.gameCell');
+
+//event listeners
+
+cell.forEach(element=>{element.addEventListener('click',markCell)});
+
+
