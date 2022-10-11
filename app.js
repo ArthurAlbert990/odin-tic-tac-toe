640cf9871273ab34d7769dc8b37a47f0ee6f1a8f
Arthur <arthur.albert990@gmail.com>
2022-10-11 20:45:42 -0300

    Add function to check winner.

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ console.log('js working');
 
 
 //saves the current board state:
-let gameboard = [];
+let gameboard = {};
 let user1Turn = true;
+//possible winning scenarios:
+const winningArray = [ 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 
  function player(name,symbol){
@@ -25,20 +36,36 @@ function markCell(e){
     if (cell.textContent == ''){
         if(user1Turn){
             cell.textContent = user1.symbol;
+            gameboard[cellIndex]=user1.symbol;
+            checkForVictory(user1, user1.symbol);
             user1Turn = false;
+
         } else{
             cell.textContent = user2.symbol;
+            gameboard[cellIndex]=user2.symbol;
+            checkForVictory(user2, user2.symbol);
             user1Turn = true;
         }
         
     }
-    checkForVictory()
+    
 }
 
-function checkForVictory(){
+function checkForVictory(user,symbol){
     // obtain cells, make array with their content
     // check against win conditions
     // return player if win.
+
+    console.log('check for victory');
+
+    for (let combination of winningArray){
+        console.log(combination)
+        if(gameboard[combination[0]]==symbol && gameboard[combination[1]]==symbol && gameboard[combination[2]]==symbol){
+            console.log(`WINNER: ${user.name}, WITH SYMBOL: ${user.symbol}`);
+            return(user);
+        }
+    }
+
     return false;
 }
 
